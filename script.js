d52af582fa9d4299c844f4c61d027bf0a3b8cb1c
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2024-12-20 12:04:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function isSameType(value1, value2) {
-  if ((isNaN(value1) && isNaN(value2))) {
+  if (Number.isNaN(value1) && Number.isNaN(value2)) {
     return true;
+  }
+  if (Number.isNaN(value1) || Number.isNaN(value2)) {
+    return false;
   }
   return typeof value1 === typeof value2;
 }
